02824cb99ae1090c4cff0652aa5f6bf03f787cf3
KimKimJungyeon <kjy20177@kookmin.ac.kr>
2022-05-15 01:58:28 +0900

    feat: group 모델 구현

--- CheckCalender/group/models.py
+++ CheckCalender/group/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    #모임 구조 - 모임 이름, 모임 번호, 모임 비밀번호, 모임 기타 정보, 생성일자
+    id = models.BigAutoField(
+        primary_key= True
+    )
+    name = models.CharField()
+    passward = models.CharField()
+    memo = models.CharField(
+        null=True
+    )
+    creation_date = models.DateTimeField()
+
+    class Meta:
+        db_table = "group"
+
